3362835309fb48dcfa09e8349205989af2a0b69d
srhartono <srhartono@ucdavis.edu>
2018-03-21 18:49:21 -0700

    V2.98
    Fixed footPeak bug

--- footPeak.pl
+++ footPeak.pl
@@ -130,7 +130,7 @@ for (my $i = 0; $i < @origFile; $i++) {
 	my $Q = new Thread::Queue;
 	my $peakFile = $origFile[$i];
 #debug
-	next if $peakFile !~ /FUS_Pos/;#CALM3_Pos/;#/(AIRN_PFC66_FORWARD_Neg|CALM3_Pos)/;
+#	next if $peakFile !~ /FUS_Pos/;#CALM3_Pos/;#/(AIRN_PFC66_FORWARD_Neg|CALM3_Pos)/;
 ##
 	my ($peakFolder, $peakFilename) = getFilename($peakFile, "folder");
 	$peakFilename =~ s/.orig$//;
