fc5b8fb65d45c21f9a25bf0511db38952c13e64d
srhartono <srhartono@ucdavis.edu>
2018-06-13 20:56:23 -0700

    V3.6k footPeak_kmer.pl: - Changed output directory from "FOOTCLUST/" to "KMER/" - Added all genes combined kmer output file (for easy copy paste to excel without having to erase header manually one by one) - changed -1 to "NA" if shuffle-able region is too small - Explicitly record in log if shuffle-able region is too small - Forgot to change name of logfile back without .temp

--- footPeak_kmer.pl
+++ footPeak_kmer.pl
@@ -42,7 +42,7 @@ die "\nUsage: $YW$0$LGN [Optional: -G (genewant)]$N $CY-n <footPeak's output fol
 my $outDir = "$footPeakFolder/FOOTCLUST/";
 
 # establish log file
-open (my $outLog, ">", "$outDir/logFile_footPeak_kmer.txt") or die "Failed to create outLog file $outDir/logFile_footPeak_kmer.txt: $!\n";
+open (my $outLog, ">", "$footPeakFolder/footPeak_kmer_logFile.txt") or die "Failed to create outLog file $footPeakFolder/footPeak_kmer_logFile.txt: $!\n";
 LOG($outLog, " \n\n$YW -------- PARSING LOG FILE -------- $N\n\n");
 
 # get .fa file from footPeakFolder and copy
